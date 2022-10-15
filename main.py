329ac40897072d13209ebaf367919c807dd010b8
Alannah Cullinane Cooney <a.cullinane-cooney@mycit.ie>
2022-10-15 20:10:31 +0100

    Added all necessary imports for predicting Apple Stocks

---
+++ main.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+from subprocess import check_output
+from tensorflow.keras.core import Dense, Activation, Dropout
+from tensorflow.keras.recurrent import LTSM
+from tensorflow.keras.models import Sequential
+from sklearn.cross_validation import train_test_split
+import time
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+from numpy import newaxis
+
+
