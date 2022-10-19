b73701ab07350a9649233ccda2467719ca967ce6
Alannah <a.cullinane-cooney@mycit.ie>
2022-10-19 21:33:24 +0100

    Testing the model

--- Apple.py
+++ Apple.py
@@ -42,3 +42,28 @@ model.add(Dense(units=1))
 
 model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x_train, y_train, epochs=25, batch_size=32)
+
+
+# Testing the model
+test_start = dt.datetime(2017, 1, 1)
+test_end = dt.datetime.now()
+
+test_data = pd.read_csv('Apple.csv', header=0)
+actual_prices = test_data['Close'].values
+
+total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
+model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
+model_inputs = model_inputs.reshape(-1, 1)
+model_inputs = scaler.fit_transform(model_inputs)
+
+x_test = []
+
+for x in range(prediction_days, len(model_inputs)):
+    x_test.append(model_inputs[x-prediction_days:x, 0])
+
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+prediction_prices = model.predict(x_test)
+prediction_prices = scaler.inverse_transform(prediction_prices)
