c3ed97da8c3fb65456d235b9f27b6eb8e8384992
Alannah <a.cullinane-cooney@mycit.ie>
2022-10-19 21:32:09 +0100

    Preparing Data for prediction

--- Apple.py
+++ Apple.py
@@ -13,3 +13,19 @@ currency = 'Euro'
 start = dt.datetime(2017, 1, 1)
 end = dt.datetime.now()
 data = pd.read_csv('Apple.csv', header=0)
+
+# Prepare Data
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
+
+prediction_days = 60
+future_day = 30
+
+x_train, y_train = [], []
+
+for x in range(prediction_days, len(scaled_data)-future_day):
+    x_train.append(scaled_data[x-prediction_days:x, 0])
+    y_train.append(scaled_data[x+future_day, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
