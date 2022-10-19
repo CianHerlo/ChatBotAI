41bb44fa0367d11bf7fb01eab60769953f65f1ad
Alannah <a.cullinane-cooney@mycit.ie>
2022-10-19 21:32:49 +0100

    Create Neural Network

--- Apple.py
+++ Apple.py
@@ -29,3 +29,16 @@ for x in range(prediction_days, len(scaled_data)-future_day):
 
 x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+# Create Neural Network
+model = Sequential()
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.add(Dense(units=1))
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, epochs=25, batch_size=32)
